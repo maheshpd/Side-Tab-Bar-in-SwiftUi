f4ed8053585f018578ae3cf9375efb7f953f7839
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-12-29 21:29:40 +0530

    Initial Commit

---
+++ Side Tab Bar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Side Tab Bar
+//
+//  Created by Mahesh Prasad on 29/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Side Tab Bar/Side_Tab_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Side_Tab_BarApp.swift
+//  Side Tab Bar
+//
+//  Created by Mahesh Prasad on 29/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Side_Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
